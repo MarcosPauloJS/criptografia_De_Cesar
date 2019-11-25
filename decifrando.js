ddfc76566fcb0b4591e6985afcf267e80baedd43
sushi <1921518530@acad.dvuadmin.net>
2019-11-25 20:16:09 -0300

    pequena alteracçao de identação

--- decifrando.js
+++ decifrando.js
@@ -1,30 +1,48 @@
 
 const cifra = { 
-     messagemOriginal:"o corvo branco vai pousar na montanha ao amanhecer",
-     chave : 1,
+     messagemOriginal:"Todos veem o que voce parece ser, mas poucos sabem o que voce realmente e",
+     chave : 56,
      messagemCriptografada: ""
      };
 
 for(let i = 0; i  < cifra.messagemOriginal.length ; i++){
-     upperCase();
-     
-     console.log("letra original:")
-     console.log(cifra.messagemOriginal.charAt(i))
-     if(cifra.messagemOriginal.charCodeAt(i) === 32){
-          cifra.messagemCriptografada += " ";
-     }else{
-          const asciiWord = cifra.messagemOriginal.charCodeAt(i) + cifra.chave;
+     upperCase(cifra.messagemOriginal);
 
-          console.log("letra criptografada:")
-          console.log(String.fromCharCode(asciiWord));
+     console.log("letra original: " + cifra.messagemOriginal.charAt(i))
+     
+     const charCodeActual = cifra.messagemOriginal.charCodeAt(i);
+    
+     console.log("char code  atual : " + charCodeActual)
+
+     
+     if(charCodeActual === 32){
+          cifra.messagemCriptografada += " ";
+          console.log("if de espaço")
+     }else{
+          const asciiWord = dicoverCharCode(charCodeActual, cifra.chave);
+          console.log("função discover retun: " , dicoverCharCode(charCodeActual, cifra.chave))
 
           const word = String.fromCharCode(asciiWord);
+
+          console.log("letra criptografada: " + word);
      
           cifra.messagemCriptografada += word;
-          console.log(cifra.messagemCriptografada);
+          console.log("const resultante : " + cifra.messagemCriptografada);
      }
 }
 
-function upperCase(){
-     cifra.messagemOriginal = cifra.messagemOriginal.toUpperCase();
+function upperCase(message){
+     cifra.messagemOriginal = message.toUpperCase();
 }
+
+
+// range 65 ao 90 (25 letras)
+function dicoverCharCode(numCode, stpes){
+     asciiCode = numCode
+     for(let i = 0; i < stpes; i++){
+          asciiCode ++
+          if(asciiCode > 90) asciiCode = 65;
+     }
+     return asciiCode;
+}
+
